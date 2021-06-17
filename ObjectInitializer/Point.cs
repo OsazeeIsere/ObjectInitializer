74bb7318ba1e7494f3cc44bf5e3d1050e6d1d1ec
Osazee Isere <oz1.concepts@gmail.com>
2021-06-18 00:02:20 +0100

    object initializer

---
+++ ObjectInitializer/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    enum PointColor
+    {
+        lightBlue, BloodRed,Gold
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+       public PointColor Color { get; set; }
+
+        //constructors
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+            Color = PointColor.lightBlue;
+        }
+        public Point(PointColor ptcolor)
+        {
+            Color = ptcolor;
+        }
+        public Point()
+            : this(PointColor.BloodRed) { }
+        //method
+        public void DisplayStatus()
+        {
+            Console.WriteLine("[{0}, {1}]", X, Y);
+            Console.WriteLine("Point is {0}",Color);
+        }
+    }
+    
+}
--- ObjectInitializer/Program.cs
+++ ObjectInitializer/Program.cs
@@ -10,6 +10,34 @@ namespace ObjectInitializer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=> fun with object init. syntax");
+            //make a point by setting each property manually
+            Point firstpoint = new Point();
+            firstpoint.X = 10;
+            firstpoint.Y = 10;
+            firstpoint.DisplayStatus();
+
+            //make a point via custom constructor
+            Point otherPoint = new Point(20, 20);
+            otherPoint.DisplayStatus();
+
+            //make a point using object init. syntax
+            Point finalPoint = new Point() { X = 30, Y = 30 };
+            finalPoint.DisplayStatus();
+            //calling a more interesting custom constructor
+            //with init. syntax
+            Point goldPoint = new Point(PointColor.Gold) { X = 90, Y = 20 };
+            goldPoint.DisplayStatus();
+
+            //CREATE and initialize a triangle
+            Rectangle myRectangle = new Rectangle
+            {
+                TopLeft = new Point { X = 10, Y = 10 },
+                BottomRight = new Point { X = 200, Y = 200 }
+            };
+            Console.ReadLine();
+
+
         }
     }
 }
